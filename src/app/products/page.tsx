fd7be93d8d798592c6f764420d3ab604ad68f17f
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-08-07 10:42:57 +0700

    finish

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -27,7 +27,7 @@ export default async function Products() {
             </div>
             <div className="mt-5"  >
                 <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5  gap-3" >
-                    {apiData.map((item: any) => (
+                    {apiData?.map((item: any) => (
                         <CardProduct key={item.id} name={item.name} image={item.images?.[0]} stock={item.total_stock} />
                     ))}
                 </div>
