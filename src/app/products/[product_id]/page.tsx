175c202ff82a499260cc048a87c25df2eff5eb9d
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-08-07 11:36:39 +0700

    create products

---
+++ src/app/products/[product_id]/page.tsx
@@ -0,0 +1,14 @@
+import DefaultLayout from '@/components/layouts/DefaultLayout'
+import React from 'react'
+
+type Props = {}
+
+const DetailProduct = (props: Props) => {
+    return (
+        <DefaultLayout>
+            <h1>Halaman Detail</h1>
+        </DefaultLayout>
+    )
+}
+
+export default DetailProduct
