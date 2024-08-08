dbd1e9b646b255e77635b36d1acdfb3244e6d1b5
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-08-08 08:37:34 +0700

    finish login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -64,6 +64,7 @@ const Login = () => {
             } else {
                 setErrorLogin('*Email atau password salah')
                 console.log(res.data);
+                setLoading(false)
             }
         })
     };
