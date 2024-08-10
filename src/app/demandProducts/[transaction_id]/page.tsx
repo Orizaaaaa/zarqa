a5290d8ff6a74351b6e1f9c0560e128796d25d45
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-08-10 11:14:48 +0700

    create page transaction

---
+++ src/app/demandProducts/[transaction_id]/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import DefaultLayout from '@/components/layouts/DefaultLayout'
+
+
+const DetailTransaction = () => {
+    return (
+        <DefaultLayout>
+            <p>babayoo transaksi</p>
+        </DefaultLayout>
+    )
+}
+
+export default DetailTransaction
